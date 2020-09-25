0ec0ec583310bd3575ed4623b7bd18155a89779c
jd <duverseaujohnny21@gmail.com>
2020-09-25 16:42:46 -0400

    rename PierreBakery.cs to Bread.cs

---
+++ PierreBakery/Models/Bread.cs
@@ -0,0 +1,16 @@
+namespace PierreBakery.Models
+{
+  public class Bread
+  {
+    public static int priceSolver(int num)
+    {
+     if (num == 1){
+       return 5;
+     } else {
+       return num * 5;
+     }
+    }
+    
+
+  }
+}
