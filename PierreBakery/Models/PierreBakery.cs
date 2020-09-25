ee689c3895c5b91240a90b40d71502f9a3c098e8
jd <duverseaujohnny21@gmail.com>
2020-09-25 14:43:34 -0400

    add if statement

---
+++ PierreBakery/Models/PierreBakery.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace PierreBakery.Models
+{
+  public class Bread
+  {
+    public static int priceSolver(int num)
+    {
+     if (num == 1){
+       return 5;
+     } else {
+       return num * 5;
+     }
+    }
+    
+
+  }
+}
