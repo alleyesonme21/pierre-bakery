e94dd9dfbe27196058d546fab3633ac3df124b90
jd <duverseaujohnny21@gmail.com>
2020-10-09 00:35:10 -0400

    first test passed

---
+++ PierreBakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierreBakery.Models;
+
+namespace PierreBakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod] 
+    public void GetCost_ReturnCost_int() {
+      Assert.AreEqual(2, Pastry.pastryCost(1));
+    }
+  }
+
+}
