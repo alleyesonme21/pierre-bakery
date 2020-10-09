a00f7ffc0c9ff4bad4229bfb11903c5e495e374c
jd <duverseaujohnny21@gmail.com>
2020-10-09 00:29:56 -0400

    indented

--- PierreBakery.Tests/ModelTests/BreadTests.cs
+++ PierreBakery.Tests/ModelTests/BreadTests.cs
@@ -1,47 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierreBakery.Models;
 
-
 namespace PierreBakery.Tests
 {
   [TestClass]
   public class BreadTests
-  {
-    [TestMethod] 
-   
-    public void GetNumber_ReturnNumber_int() {
+{
+  
+  [TestMethod] 
+  public void GetNumber_ReturnNumber_int() 
+    {
       Bread orderBread = new Bread();
       Assert.AreEqual(20, orderBread.BreadCost(4));
     }
   }
-
 }
-
-
-
-
-
-
-
-
-
-
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using PierreBakery.Models;
-
-
-// namespace PierreBakery.Tests
-// {
-//   [TestClass]
-
-//   public class BreadTests
-//   {
-//     [TestMethod]
-//     public void  GetPrice_ReturnPrice_int() {
-//       //  Bread myBreadOrder = new Bread();
-//       Assert.AreEqual(15, myBreadOrder.BreadCost(3));
-//     }
-    
-//   }
-
-// }
